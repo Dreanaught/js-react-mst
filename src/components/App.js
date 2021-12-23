814202ad77319d4fbd2459c19b856b29f3ae5c68
Dreanaught <dreanaught@gmx.net>
2021-12-23 21:01:51 +0000

    Tried react-bootstrap tabs

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Tabs from 'react-bootstrap/Tabs'
+import { Tab } from 'react-bootstrap';
 
 import ProjectView from './ProjectView';
 
@@ -7,10 +10,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Projekt</h1>
-        </header>
-        <ProjectView project={this.props.project} />
+        <h1 className="App-title">Projekt</h1>
+
+        <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
+          <Tab eventKey="home" title="Home">
+            <ProjectView project={this.props.project} />
+          </Tab>
+          <Tab eventKey="profile" title="Profile">
+          </Tab>
+          <Tab eventKey="contact" title="Contact" disabled>
+          </Tab>
+        </Tabs>
       </div>
     )
   }
