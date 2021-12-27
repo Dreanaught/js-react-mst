eb6fdd4f0a3315d1f8fac9f87f1bbf7e2463ab1f
Dreanaught <dreanaught@gmx.net>
2021-12-27 23:24:01 +0000

    auto formatting

--- src/models/Projekt.js
+++ src/models/Projekt.js
@@ -2,20 +2,21 @@ import { destroy, getParent, types } from "mobx-state-tree"
 import { Bauteil } from "./Bauteil"
 import { Raum } from "./Room"
 
-export const Projekt = types.model({
-    Norm_Außentemperatur: types.optional(types.number, -16),
-    Wäremebrückenzuschlag: types.optional(types.number, 0.1),
-    Bauteildefinitionen: types.array(Bauteil),
-    Räume: types.array(Raum)
-})
-.views(self => ({
-    get Transmissionswärmeverlust(){
-        return 0
-    },
-    get Lüftungswäremeverlust(){
-        return 0
-    },
-    get NormHeizlast(){
-        return self.Transmissionswärmeverlust + self.Lüftungswäremeverlust
-    }
-}))
+export const Projekt = types
+    .model({
+        Norm_Außentemperatur: types.optional(types.number, -16),
+        Wäremebrückenzuschlag: types.optional(types.number, 0.1),
+        Bauteildefinitionen: types.array(Bauteil),
+        Räume: types.array(Raum)
+    })
+    .views(self => ({
+        get Transmissionswärmeverlust() {
+            return 0
+        },
+        get Lüftungswäremeverlust() {
+            return 0
+        },
+        get NormHeizlast() {
+            return self.Transmissionswärmeverlust + self.Lüftungswäremeverlust
+        },
+    }))
