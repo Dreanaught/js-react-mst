dc3c07f4a488935052aa20b2ae88c97c37968b82
Dreanaught <dreanaught@gmx.net>
2021-12-27 10:41:05 +0000

    added visualization of newly added project properties

--- src/components/ProjectView.js
+++ src/components/ProjectView.js
@@ -5,6 +5,21 @@ import BauteilView from "./BauteilView"
 
 const ProjectView = ({project}) => (
     <div className="list">
+    <ul>
+        <span>Norm-Außentemperatur: {project.Norm_Außentemperatur}</span>
+    </ul>
+    <ul>
+        <span>Nom-Heizlast: {project.NormHeizlast}</span>
+    </ul>
+    <ul>
+        <span>Nom-Transmissionswäremeverlust: {project.Transmissionswärmeverlust}</span>
+    </ul>
+    <ul>
+        <span>Nom-Lüftungswäremeverlust: {project.Lüftungswäremeverlust}</span>
+    </ul>
+    <ul>
+        <span>Wäremebrückenzuschlag: {project.Wäremebrückenzuschlag}</span>
+    </ul>
         <ul>
             {project.Bauteildefinitionen.map((bauteil, idx) => (
                 <BauteilView key={idx} bauteil={bauteil}/>
