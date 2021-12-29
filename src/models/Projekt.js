0c71d5a08c2c49fd67aa0a726a4773094f7e288b
Dreanaught <dreanaught@gmx.net>
2021-12-29 17:01:20 +0000

    added calculation of "Lüftungswäremeverlust" and "Transmissionswäremeverlust"

--- src/models/Projekt.js
+++ src/models/Projekt.js
@@ -11,10 +11,10 @@ export const Projekt = types
     })
     .views(self => ({
         get Transmissionswärmeverlust() {
-            return 0
+            return self.Räume.reduce((sum, entry) => sum + entry.NormTransmissionswäremeverlust, 0)
         },
         get Lüftungswäremeverlust() {
-            return 0
+            return self.Räume.reduce((sum, entry) => sum + entry.NormLüftungswäremeverlust, 0)
         },
         get NormHeizlast() {
             return self.Transmissionswärmeverlust + self.Lüftungswäremeverlust
