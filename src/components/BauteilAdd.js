4fd6e9ae8a79c30802ec4f4c604476f2fe6ecffe
Dreanaught <dreanaught@gmx.net>
2022-01-05 20:03:49 +0000

    added units "BauteilAdd" and "BauteilAddForm"

---
+++ src/components/BauteilAdd.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react"
+import { observer } from "mobx-react"
+import { Button, Modal } from "react-bootstrap"
+import BauteilAddForm from "./BauteilAddForm";
+import { Bauteil } from "../models/Bauteil";
+
+class BauteilAdd extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleShow = this.handleShow.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+        this.handleAdd = this.handleAdd.bind(this);
+
+        this.state = {
+            show: false
+        };
+    }
+
+    handleClose() {
+        this.setState({ show: false });
+    }
+
+    handleAdd(values) {
+        alert(JSON.stringify(values, null, 2));
+        this.props.project.addBauteil(Bauteil.create(values))
+        //this.props.raum.addElement(this.state.element)
+        //this.setState({ show: false });
+    }
+
+    handleShow() {
+        this.setState({ show: true });
+    }
+
+    render() {
+        const project = this.props.project
+        return (
+            <div>
+                <Button bsstyle="primary" bssize="large" onClick={this.handleShow}>
+                    Neues U-Wert Definition
+                </Button>
+                <Modal
+                    centered
+                    show={this.state.show}
+                    onHide={this.handleClose}
+                    backdrop="static">
+                    <Modal.Header closeButton>
+                        <Modal.Title>Füge neues Element hinzu</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        {/*<ElementAddForm raum={raum}/>*/}
+                        <BauteilAddForm onSubmit={this.handleAdd}/>
+                    </Modal.Body>
+
+                    <Modal.Footer>
+                        <Button form="AddElement" type="submit" bsstyle="primary">Add</Button>
+                    </Modal.Footer>
+                </Modal>
+
+            </div>
+        )
+    }
+}
+
+export default observer(BauteilAdd)
---
+++ src/components/BauteilAddForm.js
@@ -0,0 +1,83 @@
+import React from "react"
+import { observer } from "mobx-react"
+import { Form, Button } from "react-bootstrap";
+import { useFormik } from "formik";
+import * as Yup from "yup"
+import { v4 as uuidv4 } from 'uuid';
+
+const BauteilAddForm = (props) => {
+    const formik = useFormik({
+        initialValues: {
+            id: uuidv4(),
+            Kurzbezeichner: 'AW',
+            Kommentar: '',
+            uWert: '',
+        },
+        validationSchema: Yup.object({
+            id: Yup.string().required(),
+            Kurzbezeichner: Yup.string().required(),
+            Kommentar: Yup.string(),
+            uWert: Yup.number().required().positive(),
+        }),
+        onSubmit: values => {
+            //alert(JSON.stringify(values, null, 2));
+            props.onSubmit(values)
+        }
+    });
+
+    return (
+        <Form
+            noValidate
+            onSubmit={formik.handleSubmit}
+            id="AddElement"
+        >
+            <Form.Group controlId="Kurzbezeichner">
+                <Form.Label>Typ</Form.Label>
+                <Form.Select
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.Kurzbezeichner}
+                    isInvalid={formik.touched.Kurzbezeichner && !!formik.errors.Kurzbezeichner}>
+                    <option value='AW'>AußenWand</option>
+                    <option value='AF'>AußenFenster</option>
+                    <option value='IW'>InnenWand</option>
+                    <option value='IF'>InnenFenster</option>
+                    <option value='D'>Decke</option>
+                    <option value='FB'>Fußboden</option>
+                    <option value='DA'>Dach</option>
+                </Form.Select>
+                <Form.Control.Feedback type="invalid">
+                    {formik.errors.Kurzbezeichner}
+                </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group controlId="uWert">
+                <Form.Label>U-Wert</Form.Label>
+                <Form.Control
+                    type="number"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.uWert}
+                    isInvalid={formik.touched.uWert && !!formik.errors.uWert}
+                />
+                <Form.Control.Feedback type="invalid">
+                    {formik.errors.uWert}
+                </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group controlId="Kommentar">
+                <Form.Label>Kommentar</Form.Label>
+                <Form.Control
+                    type="text"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.Kommentar}
+                    isInvalid={formik.touched.Kommentar && !!formik.errors.Kommentar}
+                />
+                <Form.Control.Feedback type="invalid">
+                    {formik.errors.Kommentar}
+                </Form.Control.Feedback>
+            </Form.Group>
+        </Form>
+    );
+}
+
+export default observer(BauteilAddForm)
